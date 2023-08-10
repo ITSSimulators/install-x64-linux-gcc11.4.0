aa8a9274d91b44ee47676cda9429cd67b42cbebb
Tony Horrobin <a.j.horrobin@its.leeds.ac.uk>
2023-08-10 16:48:16 +0100

    Initial import

---
+++ lib/osgworks-targets-noconfig.cmake
@@ -0,0 +1,86 @@
+#----------------------------------------------------------------
+# Generated CMake target import file.
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "osgwControls" for configuration ""
+set_property(TARGET osgwControls APPEND PROPERTY IMPORTED_CONFIGURATIONS NOCONFIG)
+set_target_properties(osgwControls PROPERTIES
+  IMPORTED_LINK_INTERFACE_LIBRARIES_NOCONFIG "/home/car/sim/Packages/install-x64-linux-gcc11.4.0/lib64/libosgGA.so;/home/car/sim/Packages/install-x64-linux-gcc11.4.0/lib64/libosgText.so;/home/car/sim/Packages/install-x64-linux-gcc11.4.0/lib64/libosgViewer.so;/home/car/sim/Packages/install-x64-linux-gcc11.4.0/lib64/libosgSim.so;/home/car/sim/Packages/install-x64-linux-gcc11.4.0/lib64/libosgDB.so;/home/car/sim/Packages/install-x64-linux-gcc11.4.0/lib64/libosgUtil.so;/home/car/sim/Packages/install-x64-linux-gcc11.4.0/lib64/libosg.so;/home/car/sim/Packages/install-x64-linux-gcc11.4.0/lib64/libOpenThreads.so"
+  IMPORTED_LOCATION_NOCONFIG "${_IMPORT_PREFIX}/lib/libosgwControls.so.3.00.02"
+  IMPORTED_SONAME_NOCONFIG "libosgwControls.so.3.00.02"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS osgwControls )
+list(APPEND _IMPORT_CHECK_FILES_FOR_osgwControls "${_IMPORT_PREFIX}/lib/libosgwControls.so.3.00.02" )
+
+# Import target "osgwMx" for configuration ""
+set_property(TARGET osgwMx APPEND PROPERTY IMPORTED_CONFIGURATIONS NOCONFIG)
+set_target_properties(osgwMx PROPERTIES
+  IMPORTED_LINK_INTERFACE_LIBRARIES_NOCONFIG "/home/car/sim/Packages/install-x64-linux-gcc11.4.0/lib64/libosgGA.so;/home/car/sim/Packages/install-x64-linux-gcc11.4.0/lib64/libosgText.so;/home/car/sim/Packages/install-x64-linux-gcc11.4.0/lib64/libosgViewer.so;/home/car/sim/Packages/install-x64-linux-gcc11.4.0/lib64/libosgSim.so;/home/car/sim/Packages/install-x64-linux-gcc11.4.0/lib64/libosgDB.so;/home/car/sim/Packages/install-x64-linux-gcc11.4.0/lib64/libosgUtil.so;/home/car/sim/Packages/install-x64-linux-gcc11.4.0/lib64/libosg.so;/home/car/sim/Packages/install-x64-linux-gcc11.4.0/lib64/libOpenThreads.so;/usr/lib/x86_64-linux-gnu/libGL.so;/usr/lib/x86_64-linux-gnu/libGLU.so;osgwTools"
+  IMPORTED_LOCATION_NOCONFIG "${_IMPORT_PREFIX}/lib/libosgwMx.so.3.00.02"
+  IMPORTED_SONAME_NOCONFIG "libosgwMx.so.3.00.02"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS osgwMx )
+list(APPEND _IMPORT_CHECK_FILES_FOR_osgwMx "${_IMPORT_PREFIX}/lib/libosgwMx.so.3.00.02" )
+
+# Import target "osgwQuery" for configuration ""
+set_property(TARGET osgwQuery APPEND PROPERTY IMPORTED_CONFIGURATIONS NOCONFIG)
+set_target_properties(osgwQuery PROPERTIES
+  IMPORTED_LINK_INTERFACE_LIBRARIES_NOCONFIG "/home/car/sim/Packages/install-x64-linux-gcc11.4.0/lib64/libosgGA.so;/home/car/sim/Packages/install-x64-linux-gcc11.4.0/lib64/libosgText.so;/home/car/sim/Packages/install-x64-linux-gcc11.4.0/lib64/libosgViewer.so;/home/car/sim/Packages/install-x64-linux-gcc11.4.0/lib64/libosgSim.so;/home/car/sim/Packages/install-x64-linux-gcc11.4.0/lib64/libosgDB.so;/home/car/sim/Packages/install-x64-linux-gcc11.4.0/lib64/libosgUtil.so;/home/car/sim/Packages/install-x64-linux-gcc11.4.0/lib64/libosg.so;/home/car/sim/Packages/install-x64-linux-gcc11.4.0/lib64/libOpenThreads.so;/usr/lib/x86_64-linux-gnu/libGL.so;/usr/lib/x86_64-linux-gnu/libGLU.so;osgwTools"
+  IMPORTED_LOCATION_NOCONFIG "${_IMPORT_PREFIX}/lib/libosgwQuery.so.3.00.02"
+  IMPORTED_SONAME_NOCONFIG "libosgwQuery.so.3.00.02"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS osgwQuery )
+list(APPEND _IMPORT_CHECK_FILES_FOR_osgwQuery "${_IMPORT_PREFIX}/lib/libosgwQuery.so.3.00.02" )
+
+# Import target "osgwTools" for configuration ""
+set_property(TARGET osgwTools APPEND PROPERTY IMPORTED_CONFIGURATIONS NOCONFIG)
+set_target_properties(osgwTools PROPERTIES
+  IMPORTED_LINK_INTERFACE_LIBRARIES_NOCONFIG "/home/car/sim/Packages/install-x64-linux-gcc11.4.0/lib64/libosgGA.so;/home/car/sim/Packages/install-x64-linux-gcc11.4.0/lib64/libosgText.so;/home/car/sim/Packages/install-x64-linux-gcc11.4.0/lib64/libosgViewer.so;/home/car/sim/Packages/install-x64-linux-gcc11.4.0/lib64/libosgSim.so;/home/car/sim/Packages/install-x64-linux-gcc11.4.0/lib64/libosgDB.so;/home/car/sim/Packages/install-x64-linux-gcc11.4.0/lib64/libosgUtil.so;/home/car/sim/Packages/install-x64-linux-gcc11.4.0/lib64/libosg.so;/home/car/sim/Packages/install-x64-linux-gcc11.4.0/lib64/libOpenThreads.so;/usr/lib/x86_64-linux-gnu/libGL.so"
+  IMPORTED_LOCATION_NOCONFIG "${_IMPORT_PREFIX}/lib/libosgwTools.so.3.00.02"
+  IMPORTED_SONAME_NOCONFIG "libosgwTools.so.3.00.02"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS osgwTools )
+list(APPEND _IMPORT_CHECK_FILES_FOR_osgwTools "${_IMPORT_PREFIX}/lib/libosgwTools.so.3.00.02" )
+
+# Import target "osgdb_osgobjects" for configuration ""
+set_property(TARGET osgdb_osgobjects APPEND PROPERTY IMPORTED_CONFIGURATIONS NOCONFIG)
+set_target_properties(osgdb_osgobjects PROPERTIES
+  IMPORTED_COMMON_LANGUAGE_RUNTIME_NOCONFIG ""
+  IMPORTED_LOCATION_NOCONFIG "${_IMPORT_PREFIX}/lib/osgdb_osgobjects.so"
+  IMPORTED_NO_SONAME_NOCONFIG "TRUE"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS osgdb_osgobjects )
+list(APPEND _IMPORT_CHECK_FILES_FOR_osgdb_osgobjects "${_IMPORT_PREFIX}/lib/osgdb_osgobjects.so" )
+
+# Import target "osgdb_osgwTools" for configuration ""
+set_property(TARGET osgdb_osgwTools APPEND PROPERTY IMPORTED_CONFIGURATIONS NOCONFIG)
+set_target_properties(osgdb_osgwTools PROPERTIES
+  IMPORTED_COMMON_LANGUAGE_RUNTIME_NOCONFIG ""
+  IMPORTED_LOCATION_NOCONFIG "${_IMPORT_PREFIX}/lib/osgdb_osgwTools.so"
+  IMPORTED_NO_SONAME_NOCONFIG "TRUE"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS osgdb_osgwTools )
+list(APPEND _IMPORT_CHECK_FILES_FOR_osgdb_osgwTools "${_IMPORT_PREFIX}/lib/osgdb_osgwTools.so" )
+
+# Import target "osgdb_skeleton" for configuration ""
+set_property(TARGET osgdb_skeleton APPEND PROPERTY IMPORTED_CONFIGURATIONS NOCONFIG)
+set_target_properties(osgdb_skeleton PROPERTIES
+  IMPORTED_COMMON_LANGUAGE_RUNTIME_NOCONFIG ""
+  IMPORTED_LOCATION_NOCONFIG "${_IMPORT_PREFIX}/lib/osgdb_skeleton.so"
+  IMPORTED_NO_SONAME_NOCONFIG "TRUE"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS osgdb_skeleton )
+list(APPEND _IMPORT_CHECK_FILES_FOR_osgdb_skeleton "${_IMPORT_PREFIX}/lib/osgdb_skeleton.so" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
